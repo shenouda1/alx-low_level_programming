91d733624188d3ec2c1b5abcfaf16afb0b2083d0
shenouda <shenoudaassak971@gmail.com>
2023-02-16 16:58:03 -0800

    4-puts.c correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Print string using puts() function.
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char string[52] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return (0);
 }
