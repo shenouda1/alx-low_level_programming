0311ca18ab3ae4269e4dfe7ef58e56714eb35fb8
shenouda <shenoudaassak971@gmail.com>
2023-02-16 18:04:18 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print string using fprintf function to standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
