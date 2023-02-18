a1278077aca6e536d81ad8517e89c7fea9c35071
shenouda <shenoudaassak971@gmail.com>
2023-02-17 18:33:09 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
